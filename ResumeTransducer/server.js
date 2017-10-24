7b9809da3fb6bea148e926481be3d6cf7d92f93b
harpreetsingh91 <harpreet.singhgt@gmail.com>
2017-10-23 17:53:29 -0700

    Added Express.js Server
    Added express.js server to trigger json conversion through a web request

---
+++ ResumeTransducer/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const cmd = require('node-cmd')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
+
+const Promise = require('bluebird')
+ 
+const getAsync = Promise.promisify(cmd.get, { multiArgs: true, context: cmd })
+ 
+getAsync("java -cp 'bin/*:../GATEFiles/lib/*:../GATEFiles/bin/gate.jar:lib/*' code4goal.antony.resumeparser.ResumeParserProgram '/Users/harpreetsingh/Google Drive/Resume/HarpreetSinghResume.pdf'  /Users/harpreetsingh/Desktop/harpreetresume.json").then(data => {
+  console.log('cmd data', data)
+}).catch(err => {
+  console.log('cmd err', err)
+})
+
